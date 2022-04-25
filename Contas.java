a23cd7b132c508ea5e5146fab5e7e665ac7eb29f
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-04-25 17:04:06 +0000

    salvo

---
+++ Contas.java
@@ -0,0 +1,27 @@
+public class Contas
+{   
+    private int numero;
+    private double saldo;
+
+    public void setNumero(int numero)
+    {
+        this.numero = numero;
+    }
+
+    public void setSaldo(int saldo)
+    {
+        this.saldo = saldo;
+    }
+
+    //acessar com métodos píblicos
+
+    public double getSaldo()
+    {
+        return this.saldo;
+    }
+
+    public int getNumero()
+    {
+        return this.numero;
+    }
+}
---
+++ TestaConta.java
@@ -0,0 +1,10 @@
+public class TestaConta 
+{
+    public static void main(String[] args) 
+    {
+       Contas c = new Contas();
+
+       System.out.println(c.getNumero());
+
+    }    
+}
